396dd53bb96ac17dfa028710b58e19efcd51546e
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2021-01-29 19:15:18 +0900

    프로젝트 명 변경 및 테스트 컨트롤러 추가

---
+++ src/main/java/com/ryu/consumer/biggwang/TestController.java
@@ -0,0 +1,20 @@
+package com.ryu.consumer.biggwang;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        LocalDateTime now = LocalDateTime.now();
+        log.warn("########################## now:" + now);
+        return "test:" + now;
+    }
+
+}
