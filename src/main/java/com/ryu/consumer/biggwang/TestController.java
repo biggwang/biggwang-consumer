1d28f3bb4d35d3abcb78dbf84ca74beaf2aecb53
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2021-02-01 23:47:26 +0900

    마스터 커밋

--- src/main/java/com/ryu/consumer/biggwang/TestController.java
+++ src/main/java/com/ryu/consumer/biggwang/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         LocalDateTime now = LocalDateTime.now();
-        log.warn("########################## now:" + now);
+        log.warn("######################## now:" + now);
         return "test:" + now;
     }
 
