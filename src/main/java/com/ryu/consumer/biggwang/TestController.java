52ca45efac726b51ec970c4efc3c6ae5d27a01ac
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2021-02-02 23:27:33 +0900

    테스트

--- src/main/java/com/ryu/consumer/biggwang/TestController.java
+++ src/main/java/com/ryu/consumer/biggwang/TestController.java
@@ -13,8 +13,8 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         LocalDateTime now = LocalDateTime.now();
-        log.warn("######################## gogo8:" + now);
-        return "gogo8:" + now;
+        log.warn("######################## gogo9:" + now);
+        return "gogo9:" + now;
     }
 
 }
